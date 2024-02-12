395d88a9cd730e9fbc64392ed3fde6f502080411
shamoleg <omshamanin@gmail.com>
2024-02-12 03:29:45 +0300

    Шаблонная реализация

--- include/PathFinder.h
+++ include/PathFinder.h
@@ -1,34 +1,26 @@
 //
 // Created by sham on 2/11/24.
 //
-#include <vector>
-#include <limits>
-#include <cmath>
-
 #ifndef TESTING_TASK_PATHFINDER_H
 #define TESTING_TASK_PATHFINDER_H
 
+#include <cmath>
+#include <vector>
+#include <limits>
+#include <optional>
+#include <iostream>
+#include <algorithm>
+
 namespace pf {
 
-template<typename PointType>
+template<typename RouteType>
 class IPathFinder {
 public:
     virtual ~IPathFinder() = default;
 
-    virtual void setNodes(const std::vector<PointType> &route) = 0;
+    virtual void setNodes(const RouteType &route) = 0;
     virtual float getShortestTime() = 0;
-    virtual std::vector<PointType> getPathWithShortestTime() = 0;
-};
-
-struct WayPoint2D{
-    struct Position2D {
-        float x;
-        float y;
-    };
-    Position2D pos2D;
-    float penaltySkipping;
-
-    static float calcDistance(const WayPoint2D& p1, const WayPoint2D& p2);
+    virtual RouteType getPathWithShortestTime() = 0;
 };
 
 struct PathFinderParams{
@@ -36,22 +28,95 @@ struct PathFinderParams{
     float loadingTime;
 };
 
-using GraphTable = std::vector<std::vector<std::pair<int, float>>>;
-
-class PathFinder : public IPathFinder<WayPoint2D> {
+template<typename RouteType>
+class PathFinder : public IPathFinder<RouteType> {
 public:
-    explicit PathFinder(PathFinderParams params);
-    void setNodes(const std::vector<WayPoint2D> &route) final;
-    float getShortestTime() final {return {};};
-    std::vector<WayPoint2D> getPathWithShortestTime() final{return {};};
+    explicit PathFinder(PathFinderParams params):
+            params(params),
+            shortestTime(std::numeric_limits<float>::max())
+            {};
+
+    void setNodes(const RouteType &route) final{
+        std::vector<float> timeToPoint(route.size(), std::numeric_limits<float>::max());
+        std::vector<int> previous(route.size(), -1);
+
+        timeToPoint[0] = 0;
+        for (int currPoint = 0; currPoint < route.size(); currPoint++) {
+            int penaltySum = 0;
+            for (int nextPoint = currPoint + 1; nextPoint < route.size(); nextPoint++) {
+                float cost = calcDistance(route[currPoint], route[nextPoint])
+                             / params.robotVelocity + params.loadingTime + static_cast<float>(penaltySum);
+                if (timeToPoint[currPoint] + cost < timeToPoint[nextPoint]) {
+                    timeToPoint[nextPoint] = timeToPoint[currPoint] + cost;
+                    previous[nextPoint] = currPoint;
+                }
+                penaltySum += (route[nextPoint].penaltySkipping);
+            }
+        }
+        shortestTime = timeToPoint.back();
+
+        int prevIndex = previous.back();
+        shortestPath.clear();
+        shortestPath.push_back(route.back());
+        while (prevIndex != -1){
+            shortestPath.push_back(route[prevIndex]);
+            prevIndex = previous[prevIndex];
+        }
+        std::reverse(shortestPath.begin(), shortestPath.end());
+    };
+
+    float getShortestTime() final {
+        return shortestTime;
+    };
+
+    RouteType getPathWithShortestTime() {
+            return shortestPath;
+    };
 
 private:
+
     PathFinderParams params;
     float shortestTime;
-    std::vector<WayPoint2D> shortestPath;
-    GraphTable transitionsTable;
+    RouteType shortestPath;
 };
 
+struct WayPoint2D{
+    struct Position2D {
+        int x;
+        int y;
+    };
+    Position2D pos2D;
+    int penaltySkipping;
+};
+
+float calcDistance(const WayPoint2D& p1, const WayPoint2D& p2){
+    return std::sqrt(static_cast<float>(std::pow(p1.pos2D.x - p2.pos2D.x, 2) +
+                                           std::pow(p1.pos2D.y - p2.pos2D.y, 2)));
+}
+
+constexpr WayPoint2D WayPointZeroPose{0, 0, 0};
+constexpr WayPoint2D WayPointFinishPose{100, 100, 0};
+
+using RouteWayPoint2D = std::vector<WayPoint2D>;
+using PathFinderWayPoint2D = PathFinder<RouteWayPoint2D>;
+
+std::optional<RouteWayPoint2D> createPathFinderWayPoint2D(std::istream &stream){
+    int numberOfPointInIO = 0;
+    stream >> numberOfPointInIO;
+    if (numberOfPointInIO == 0){
+        return std::nullopt;
+    }
+
+    std::vector<WayPoint2D> route(numberOfPointInIO + 2);
+    route[0] = WayPointZeroPose;
+    for(int i = 1; i <= numberOfPointInIO; i++){
+        stream >> route[i].pos2D.x >> route[i].pos2D.y >> route[i].penaltySkipping;
+    }
+    route.back() = WayPointFinishPose;
+
+    return route;
+}
 
 }
+
 #endif //TESTING_TASK_PATHFINDER_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,23 @@
 // Created by sham on 2/11/24.
 //
 #include <iostream>
-#include <memory>
 #include <PathFinder.h>
 
-class TestRobot{
-    // ...
-    // ...
-    std::unique_ptr<pf::IPathFinder<pf::Position2D>> pathFinder;
-};
-
 int main(){
-    std::vector<pf::WayPoint2D> v{
-            {0,0,0},
-            {30, 30, 90},
-            {60, 60, 80},
-            {10, 90, 10},
-            {100,100,0},
-    };
-    pf::WayPoint s{1,1,1};
+    while (true){
+        auto creatorPathFinder = pf::createPathFinderWayPoint2D(std::cin);
+        if(creatorPathFinder.has_value()){
+
+            float robotVelocity = 2;
+            float loadingTime = 10;
+            pf::PathFinderParams params{robotVelocity,loadingTime};
+            pf::PathFinderWayPoint2D pathFinder(params);
+
+            pathFinder.setNodes(creatorPathFinder.value());
+
+            std::cout << std::roundf(pathFinder.getShortestTime() * 1000)/1000 << std::endl;
+        } else{
+            break;
+        }
+    }
 }
