a1f842ac4e7be76d41787666fd2cbb4dfce91e47
shamoleg <omshamanin@gmail.com>
2024-02-17 02:08:09 +0300

    Отредактировал main

--- include/DataStructures.h
+++ include/DataStructures.h
@@ -7,65 +7,76 @@
 #ifndef TESTING_TASK_DATASTRUCTURES_H
 #define TESTING_TASK_DATASTRUCTURES_H
 
-struct Point2D {
-    double x;
-    double y;
+namespace pf {
 
-    Point2D() : x(0.0), y(0.0) {}
-    Point2D(double x, double y) : x(x), y(y){}
-};
+    struct Point2D {
+        double x;
+        double y;
 
-struct Point3D {
-    double x;
-    double y;
-    double z;
+        Point2D() : x(0.0), y(0.0) {}
 
-    Point3D() : x(0.0), y(0.0), z(0.0) {}
-    Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
-};
+        Point2D(double x, double y) : x(x), y(y) {}
+    };
 
-struct Quaternion {
-    double x;
-    double y;
-    double z;
-    double w;
+    struct Point3D {
+        double x;
+        double y;
+        double z;
 
-    Quaternion() : x(0.0), y(0.0), z(0.0), w(1.0) {}
-    Quaternion(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
-};
+        Point3D() : x(0.0), y(0.0), z(0.0) {}
 
-struct Pose3D {
-    Point3D location;
-    Quaternion orientation;
+        Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
+    };
 
-    Pose3D() : location(Point3D()), orientation() {}
-    Pose3D(Point3D location, Quaternion orientation) : location(location), orientation(orientation) {}
-};
+    struct Quaternion {
+        double x;
+        double y;
+        double z;
+        double w;
 
-struct WayPoint {
-    Point2D pose2D;
-    double skipPenalty;
+        Quaternion() : x(0.0), y(0.0), z(0.0), w(1.0) {}
 
-    WayPoint() : pose2D(), skipPenalty(0.0) {}
-    WayPoint(Point2D pose2D, double skipPenalty) : pose2D(pose2D), skipPenalty(skipPenalty) {}
-};
+        Quaternion(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
+    };
 
-struct MapMeta {
-    double width;
-    double height;
-    std::string mapName;
-    Pose3D poseInRealWorld;
+    struct Pose3D {
+        Point3D location;
+        Quaternion orientation;
 
-    MapMeta() : width(0.0), height(0.0), poseInRealWorld() {};
-    MapMeta(double width, double height, std::string mapName, Pose3D poseInRealWorld = Pose3D())
-            : width(width), height(height), mapName(std::move(mapName)), poseInRealWorld(poseInRealWorld) {}
-};
+        Pose3D() : location(Point3D()), orientation() {}
 
-class IGraph{
-public:
-    virtual std::vector<int> getNeighborById(int id) const = 0;
-    virtual size_t getSizeUniqNode () const = 0;
-    virtual ~IGraph() = default;
-};
+        Pose3D(Point3D location, Quaternion orientation) : location(location), orientation(orientation) {}
+    };
 
+    struct WayPoint {
+        Point2D pose2D;
+        double skipPenalty;
+
+        WayPoint() : pose2D(), skipPenalty(0.0) {}
+
+        WayPoint(Point2D pose2D, double skipPenalty) : pose2D(pose2D), skipPenalty(skipPenalty) {}
+    };
+
+    struct MapMeta {
+        double width;
+        double height;
+        std::string mapName;
+        Pose3D poseInRealWorld;
+
+        MapMeta() : width(0.0), height(0.0), poseInRealWorld() {};
+
+        MapMeta(double width, double height, std::string mapName, Pose3D poseInRealWorld = Pose3D())
+                : width(width), height(height), mapName(std::move(mapName)), poseInRealWorld(poseInRealWorld) {}
+    };
+
+    class IGraph {
+    public:
+        virtual std::vector<int> getNeighborById(int id) const = 0;
+
+        virtual size_t getSizeUniqNode() const = 0;
+
+        virtual ~IGraph() = default;
+    };
+
+}
 #endif //TESTING_TASK_DATASTRUCTURES_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,95 @@
 //
 
 #include "PathFinder.h"
+#include "Algorithm.h"
+
 
 #include <sstream>
 #include <iostream>
 #include <cassert>
 
+void testingTask(std::istream& in, std::ostream& out);
+void testPathFinder();
+void testDijkstraAlgorithm2();
+
+int main(){
+    testingTask(std::cin, std::cout);
+}
+
 void testingTask(std::istream& in, std::ostream& out){
-//    while (true){
-//        auto creatorPathFinder = pf::createRouteWayPoint(in);
-//        if(creatorPathFinder.has_value()){
-//
-//            float robotVelocity = 2;
-//            float loadingTime = 10;
-//            pf::PathFinderParams params{robotVelocity,loadingTime};
-//            pf::PathFinder pathFinder(params);
-//
-//            pathFinder.calculatePath(creatorPathFinder.value());
-//
-//            out << std::roundf(pathFinder.getShortestTime() * 1000)/1000 << std::endl;
-//        } else{
-//            break;
-//        }
-//    }
+    while (true){
+        int numNodes = 0;
+        in >> numNodes;
+        if(numNodes == 0){
+            break;
+        }
+
+        // по сути при создании можно было бы считать вес каждого перехода,
+        // но решил оставить эту логику только в алгоритме
+        auto dag = pf::DirectedAcyclicGraph(numNodes+2);
+        for(int i = 0; i < dag.getSizeUniqNode(); i++){
+            for(int j = i+1; j < dag.getSizeUniqNode(); j++){
+                dag.addDirectedEdge(i,j);
+            }
+        }
+
+        std::vector<pf::WayPoint> route;
+        route.push_back({{0,0},0});
+        for(int i = 0; i < numNodes; i++){
+            pf::WayPoint point;
+            in >> point.pose2D.x >> point.pose2D.y >> point.skipPenalty;
+            route.push_back(point);
+        }
+        route.push_back({{100,100},0});
+
+        // в этом функторе реализованна логика подсчета стоймости перехода от точки к точке
+        // При изменении условий местности, например появления препятствий, данный класс можно заменить
+        // на другой
+        pf::PathCalculatorWithPenalty pc({2, 10}, route);
+
+
+        // не смог придумать алгоритм который бы был лучше O(V + E), где V - количество вершин, а E - количество рёбер
+        // у дейкстры асимптотика хуже так как идет добавление элемента в приоритетную очередь
+        auto [resRoute, cost] =
+                pf::algorithms::weightedTopologicalSort(0, route.size()-1 , dag,  pc);
+        cost = std::round(1000 * cost) / 1000;
+        out << cost << std::endl;
+    }
+}
+
+
+void testDijkstraAlgorithm2() {
+    auto gridGraph = pf::GridGraph(5, 5, {});
+    auto goal = gridGraph.getIdByRowCol(3, 4);
+    gridGraph.setObstacles(2,0) ;
+
+    auto awesomeFunc = [](float, float ) -> float  { return 1;};
+
+    auto [route, cost] = pf::algorithms::dijkstraAlgorithm2(0, goal, gridGraph, awesomeFunc);
+    std::cout << "this is cost " << cost << std::endl;
+    for(const auto& id : route){
+        auto [row, col] = gridGraph.getRowColById(id);
+        std::cout << row << " " << col << std::endl;
+    }
 }
 
 void testPathFinder(){
     std::stringstream out;
     std::stringstream in;
     in << "1\n"
-           "50 50 20\n"
-           "3\n"
-           "30 30 90\n"
-           "60 60 80\n"
-           "10 90 100\n"
-           "3\n"
-           "30 30 90\n"
-           "60 60 80\n"
-           "10 90 10\n"
-           "0";
+          "50 50 20\n"
+          "3\n"
+          "30 30 90\n"
+          "60 60 80\n"
+          "10 90 100\n"
+          "3\n"
+          "30 30 90\n"
+          "60 60 80\n"
+          "10 90 10\n"
+          "0";
 
     testingTask(in, out);
     std::string referentResult = "90.711\n156.858\n110.711\n";
     // Тут лучше использовать GoogleTest, но не хотел тянуть зависимость
     assert(referentResult == out.str());
-}
-
-int main(){
-    testingTask(std::cin, std::cout);
 }
