e947685ce4bceca4511cf74efcb82e48edc87fcd
shamoleg <omshamanin@gmail.com>
2024-02-17 01:18:06 +0300

    Добавлена топологическая сортировка

--- include/Algorithm.h
+++ include/Algorithm.h
@@ -14,7 +14,16 @@
 
 namespace pf::algorithms {
 
+    void depthFirstSearch(int nodeId, const IGraph& graph, std::vector<int>& visited, std::stack<int>& order);
+
+    std::stack<int> topologicalSort(const IGraph &graph);
+
+
     using ShortPathWhithCost = std::pair<std::vector<int>, double>;
+
+    ShortPathWhithCost weightedTopologicalSort(int idPointStart, int idPointTarget,
+                                               const IGraph& graph,
+                                               const std::function<float(int, int)>& calculateTransitionCosts);
 
     ShortPathWhithCost dijkstraAlgorithm2(int idPointStart, int idPointTarget,
                                           const IGraph &graph,
--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -7,6 +7,72 @@
 
 namespace pf::algorithms{
 
+    //TODO: добавить проверку на валидность входа
+    void depthFirstSearch(const int nodeId, const IGraph& graph, std::vector<int>& visited, std::stack<int>& order){
+        for (const auto adjacentNode: graph.getNeighborById(nodeId)) {
+            if (!visited[adjacentNode]) {
+                visited[adjacentNode] = 1;
+                depthFirstSearch(adjacentNode, graph, visited, order);
+            }
+        }
+        order.push(nodeId);
+    }
+
+    //TODO: добавить проверку на валидность входа
+    std::stack<int> topologicalSort(const IGraph& graph) {
+
+        std::stack<int> order;
+        std::vector<int> visited(graph.getSizeUniqNode(), 0);
+
+        for (int nodeId = 0; nodeId < graph.getSizeUniqNode(); ++nodeId) {
+            if (!visited[nodeId]) {
+                depthFirstSearch(nodeId, graph, visited, order);
+            }
+        }
+        return order;
+    }
+
+    //TODO: добавить проверку на валидность входа
+    ShortPathWhithCost weightedTopologicalSort(const int idPointStart, const int idPointTarget,
+                                               const IGraph& graph,
+                                               const std::function<float(int, int)>& calculateTransitionCosts) {
+
+        auto order = topologicalSort(graph);
+
+        std::vector<double> costToPoint(graph.getSizeUniqNode(), std::numeric_limits<double>::max());
+        std::vector<int> prev(graph.getSizeUniqNode(), std::numeric_limits<int>::max());
+        costToPoint[idPointStart] = 0.0;
+
+        while (!order.empty()) {
+            auto currId = order.top();
+            order.pop();
+            if(currId < idPointStart) continue;
+
+            for (const auto nextId : graph.getNeighborById(currId)) {
+                const auto transitionCost = calculateTransitionCosts(currId, nextId) ;
+                const auto newCost = costToPoint[currId] + transitionCost;
+
+                if (newCost < costToPoint[nextId]) {
+                    costToPoint[nextId] = newCost;
+                    prev[nextId] = currId;
+                }
+            }
+        }
+
+        if(costToPoint[idPointTarget] == std::numeric_limits<double>::infinity()){
+            return {{},costToPoint[idPointTarget]};
+        }
+
+        std::vector<int> shortestPath;
+        shortestPath.push_back(idPointTarget);
+        for (int prevIndex = prev.back(); prevIndex != std::numeric_limits<int>::max(); prevIndex = prev[prevIndex]) {
+            shortestPath.push_back(prevIndex);
+        }
+        std::reverse(shortestPath.begin(),shortestPath.end());
+
+        return {shortestPath, costToPoint[idPointTarget]};
+    }
+
     ShortPathWhithCost dijkstraAlgorithm2(const int idPointStart, const int idPointTarget,
                                           const IGraph& graph,
                                           const std::function<float(int, int)>& calculateTransitionCosts) {
