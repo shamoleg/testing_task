346bfdb89925baaa29039748788d9bbd70143b50
shamoleg <omshamanin@gmail.com>
2024-02-16 22:40:17 +0300

    Добавил два графа с которыми может работать алгоритм а так же вынес из main логику подсчета времени между точками в функтор

--- include/PathFinder.h
+++ include/PathFinder.h
@@ -12,8 +12,63 @@
 #include <algorithm>
 #include <queue>
 
+#include "DataStructures.h"
+
 namespace pf {
 
+    class DirectedAcyclicGraph : public IGraph{
+    public:
+        explicit DirectedAcyclicGraph(size_t numOfNode);
+        std::vector<int> getNeighborById(int id) const final;
+        size_t getSizeUniqNode () const final;
+
+        void addDirectedEdge(int from, int to);
+
+    private:
+        std::vector<std::vector<std::pair<double, int>>> data;
+    };
+
+    class GridGraph : public IGraph{
+    public:
+        GridGraph(int rows, int cols, MapMeta mapMeta)
+                : rows(rows), cols(cols), data(rows * cols, true), mapMeta(std::move(mapMeta)) {};
+
+        void setObstacles(int row, int col);
+
+        int getIdByRowCol(int row, int col) const;
+        std::pair<int, int> getRowColById(int id) const;
+        std::vector<int> getNeighborById(int id) const final;
+        size_t getSizeUniqNode() const final;
+
+    private:
+        int rows;
+        int cols;
+        std::vector<bool> data;
+
+        MapMeta mapMeta;
+    };
+
+    struct RobotParam{
+        double speed = 2.0;
+        double loadTime = 10.0;
+    };
+
+    class PathCalculatorWithPenalty {
+    private:
+        double skipPenaltySum = 0;
+        RobotParam param;
+        std::vector<WayPoint> route;
+
+    public:
+        explicit PathCalculatorWithPenalty( std::vector<WayPoint> route) : skipPenaltySum(0), route(std::move(route)){};
+
+        PathCalculatorWithPenalty(RobotParam param, std::vector<WayPoint> route) :
+                skipPenaltySum(0),param(param), route(std::move(route)) {};
+
+        static double calculateTransition(const WayPoint& w1, const WayPoint& w2);
+        double calculateSkipCostSum(int id1, int id2);
+        double operator()(int id1, int id2);
+    };
 }
 
 #endif //TESTING_TASK_PATHFINDER_H
--- src/PathFinder.cpp
+++ src/PathFinder.cpp
@@ -4,6 +4,91 @@
 #include "PathFinder.h"
 
 namespace pf{
+    pf::DirectedAcyclicGraph::DirectedAcyclicGraph(size_t numOfNode) {
+        data.reserve(numOfNode);
+        for(size_t i = 0; i < numOfNode; i++ ){
+            data.emplace_back();
+        }
+    }
 
+    std::vector<int> DirectedAcyclicGraph::getNeighborById(int id) const  {
+        std::vector<int> neighbors;
+
+        if (id >= 0 && static_cast<size_t>(id) < data.size()) {
+            for (const auto [_, neighbor] : data[id]) {
+                neighbors.push_back(neighbor);
+            }
+        }
+        return neighbors;
+    }
+
+    size_t DirectedAcyclicGraph::getSizeUniqNode() const {
+        return data.size();
+    }
+
+    void DirectedAcyclicGraph::addDirectedEdge(int from, int to) {
+        if (from >= 0 && from < data.size() && to >= 0 && to < data.size()) {
+            data[from].emplace_back(0, to);
+        }
+    };
+
+    int GridGraph::getIdByRowCol(int row, int col) const {
+        return row * cols + col;
+    }
+
+    std::pair<int, int> GridGraph::getRowColById(int id) const {
+        return {id/rows, id%cols};
+    }
+
+    void GridGraph::setObstacles(int row, int col) {
+        int id = getIdByRowCol(row, col);
+        if (id >= 0 && id < data.size()) {
+            data[id] = false;
+        }
+    }
+
+    std::vector<int> GridGraph::getNeighborById(int id) const {
+        std::vector<int> neighbors;
+
+        int row = id / cols;
+        int col = id % cols;
+        auto checkNeighbor = [&](int r, int c) {
+            int neighborId = r * cols + c;
+            if (r >= 0 && r < rows && c >= 0 && c < cols && data[neighborId]) {
+                neighbors.push_back(neighborId);
+            }
+        };
+
+        checkNeighbor(row - 1, col);
+        checkNeighbor(row + 1, col);
+        checkNeighbor(row, col - 1);
+        checkNeighbor(row, col + 1);
+
+        return neighbors;
+    }
+
+    size_t GridGraph::getSizeUniqNode() const {
+        return rows * cols;
+    }
+
+    double PathCalculatorWithPenalty::calculateTransition(const WayPoint &w1, const WayPoint &w2) {
+        return sqrt((pow(w1.pose2D.x - w2.pose2D.x, 2.0) + pow(w1.pose2D.y - w2.pose2D.y, 2.0)));
+    }
+
+    double PathCalculatorWithPenalty::calculateSkipCostSum(const int id1, const int id2) {
+        if(id2 - id1 == 1){
+            skipPenaltySum = 0;
+        }
+        double res = skipPenaltySum;
+        skipPenaltySum += route[id2].skipPenalty;
+        return res;
+    }
+
+    double PathCalculatorWithPenalty::operator()(const int id1, const int id2) {
+        double cost = calculateTransition(route[id1], route[id2]) / param.speed;
+        cost += calculateSkipCostSum(id1, id2);
+        cost += param.loadTime;
+        return cost;
+    }
 
 }
